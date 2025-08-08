d48e3cdb504a621342bf4574f25c767001325632
Estevanzc <estevan.zimermann@gmail.com>
2025-08-07 22:55:09 -0300

    header

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\GeneralController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/", [GeneralController::class, "index"])->name("index");
 Route::resource('/movie', MovieController::class);
 Route::resource('/user', UserController::class);
 Route::get("/login", [UserController::class, "login"])->name("login");//adicionei só para poder acessar as páginas de login/logon
