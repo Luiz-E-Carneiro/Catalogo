1513719ca86b88f1679d3d065e074ee91a08b1f2
Estevanzc <estevan.zimermann@gmail.com>
2025-08-10 13:22:54 -0300

    email_verifyed at

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,6 @@ class User extends Authenticatable
     protected $fillable = [
         "name",
         "email",
-        "email_verified_at",
         "password",
         "profile",
         "role",
