3788a91a3ff81e6474591974044c4f6651f8f725
EstevanZC <estevan.zimermann@gmail.com>
2025-08-12 11:11:10 -0300

    official trailer

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -15,6 +15,20 @@ class MovieFactory extends Factory
 
         $covers = collect(File::files($coversDir))->map(fn($file) => 'covers/' . $file->getFilename());
         $banners = collect(File::files($bannersDir))->map(fn($file) => 'banners/' . $file->getFilename());
+        $trailer_links = collect([
+            "https://youtu.be/TcMBFSGVi1c?si=F-76Gs3zqWCgA48r",
+            "https://youtu.be/W_H7_tDHFE8?si=yg-ykhDthwOpM_n9",
+            "https://youtu.be/CwXOrWvPBPk?si=o9SZDX4LlCP9_caE",
+            "https://youtu.be/mqqft2x_Aa4?si=XEvxWnWOoH35ACoj",
+            "https://youtu.be/xjDjIWPwcPU?si=02U_NkpqeHllO9fE",
+            "https://youtu.be/i6avfCqKcQo?si=Daofuk8T4dtN5OkX",
+            "https://youtu.be/eTjDsENDZ6s?si=OoEpUC6EVeL3SfI3",
+            "https://youtu.be/BcDK7lkzzsU?si=sIpkRVrZB-08Lx_v",
+            "https://youtu.be/zAGVQLHvwOY?si=0cVhUuuh9FmnU_f_",
+            "https://youtu.be/lg5hj2c5Nkk?si=2LI-e7C3bEVsgztV",
+            "https://youtu.be/E4K7JgPJ8-s?si=9oKXDr4hUO-KUxfT",
+            "https://youtu.be/DDgoDooApwM?si=uGLMWhfvQcXRSGA1",
+        ]);
 
         return [
             'title' => fake()->sentence(3),
@@ -22,7 +36,7 @@ class MovieFactory extends Factory
             'publisher' => fake()->sentence(2),
             'year' => fake()->numberBetween(1980, 2025),
             'rating' => fake()->randomFloat(1, 0, 10), // 0.0 a 10.0
-            'link' => 'https://www.youtube.com/watch?v=' . $this->faker->regexify('[A-Za-z0-9_-]{11}'),
+            'link' => $trailer_links->random(),
             'cover' => $covers->isNotEmpty() ? $covers->random() : null,
             'banner' => $banners->isNotEmpty() ? $banners->random() : null,
             'category_id' => Category::inRandomOrder()->first()->id ?? 1,
